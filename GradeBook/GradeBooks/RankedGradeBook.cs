e9a3a558bea1ad01fe4e5a05af11e4a7a5debc83
Sylvain <sly.pims@gmail.com>
2020-01-20 10:19:32 +1300

    Step #4

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -34,5 +34,21 @@ namespace GradeBook.GradeBooks
             else 
                 return 'F';
         }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calulate a student's overall grade.");
+
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calulate a student's overall grade.");
+
+            base.CalculateStudentStatistics(name);
+        }
     }
 }
